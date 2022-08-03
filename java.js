e2da88852c2af464717c76d07ff36e75ecd62627
Tyler Fribourg <tyler0153@gmail.com>
2022-08-03 16:19:37 -0400

    Reset grid functionality, background color added

--- java.js
+++ java.js
@@ -1,27 +1,48 @@
 const container = document.querySelector('.container')
-const sizeOf = 16
+let sizeOf = prompt('pick a size')
 
-// create div function
+//reset button
+const btn =document.querySelector('button')
+
+btn.addEventListener('click', () => {
+    let newGrid = Number(prompt('enter a grid size'))
+    
+    while(newGrid > 100) 
+    
+    newGrid = Number(prompt('enter a num < 100'))
+    
+
+    const wrapper = document.querySelector('.wrapper')
+    wrapper.remove()
+    
+    createGrid(newGrid)
+    
+})
+
+// create grid function
 const createGrid = (amtGrids) =>{
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
     for (let i = 0; i < amtGrids; i++ ){
         const row = document.createElement('div')
         row.classList.add('grid-row')
 
      for (let j=0; j < amtGrids; j++){ 
-        const WandH = 960 / sizeOf
+        let WandH = 1080 / sizeOf
         const gridBox = document.createElement('div')
         gridBox.classList.add('grid-box')
         gridBox.style.width= `${WandH}px`
         gridBox.style.height= `${WandH}px`
 
         gridBox.addEventListener('mouseenter', () => {
-            gridBox.style.backgroundColor= 'black'
+            gridBox.style.backgroundColor= 'blueviolet';
         })
 
         row.appendChild(gridBox)
     }
-    container.appendChild(row)
+    wrapper.appendChild(row)
 }
+container.appendChild(wrapper)
 }
 createGrid(sizeOf)
 
